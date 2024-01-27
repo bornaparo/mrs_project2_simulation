2fd429813aad962a7797a667195d2d5538cde2ac
enio <enio.krizman@fer.hr>
2024-01-27 12:50:01 +0100

    prvi pokusaj switching

--- scripts/calc_neighbours_node.py
+++ scripts/calc_neighbours_node.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 import rospy
+from std_msgs.msg import String
 from nav_msgs.msg import Odometry
 from mrs_project_simulation.msg import Neighbours
 import numpy as np
 from typing import List
 import rospkg
+import math
 
 class CalcNeighboursNode():
     def __init__(self):
@@ -14,6 +16,7 @@ class CalcNeighboursNode():
 
         self.num_robots = rospy.get_param('~num_robots', default=3) #int
         package = rospkg.RosPack().get_path('mrs_project2_simulation')
+        #adjacency matrix
         adj_mat_file = rospy.get_param('~adjacency_params', default="empty_triangle_random_adj")
         self.adjacency_matrix = np.loadtxt(package + f'/adjacency_params/{adj_mat_file}.txt', delimiter=" ", comments="#")
 
@@ -23,6 +26,8 @@ class CalcNeighboursNode():
         [rospy.Subscriber(f'/robot_{robot_id}/odom', Odometry, self.robots_odom_callback, callback_args=robot_id) for robot_id in range(self.num_robots)] #callback_args (any) - additional arguments to pass to the callback. This is useful when you wish to reuse the same callback for multiple subscriptions.
         self.rate = rospy.Rate(PUB_RATE) #frekvencija kojom publisha poruke, nece affectat to da missas poruke koje dobivas, ovo utjece samo na publishanje
 
+        self.switch = True
+        self.radius_value = 0.2
 
     def robots_odom_callback(self, robot_odom, robot_id):
         self.odoms[robot_id] = robot_odom
@@ -31,23 +36,49 @@ class CalcNeighboursNode():
         while not rospy.is_shutdown():
             neighbours_dict = {f"robot_{i}": [] for i in range(self.num_robots)}
             
-            if None not in self.odoms: #ako je popunjena lista sa ne None vrijednostima tj popunjena je odom podacima
-                for i in range(self.num_robots):
-                    for j in range(self.num_robots):
-                        if i == j:
-                            continue
+            if self.switch == False:
+                if None not in self.odoms: #ako je popunjena lista sa ne None vrijednostima tj popunjena je odom podacima
+                    for i in range(self.num_robots):
+                        for j in range(self.num_robots):
+                            if i == j:
+                                continue
+                            if self.adjacency_matrix[i, j]: #ako i-ti šalje j-tom svoju odometriju (poziciju) (ako self.adjacency_matrix[i, j] nije 0)
+                                self.odoms[i].child_frame_id = str(i) #da bi mogao vidjet ko je to poslao i onda dobit od njega zetu, a da ne moram mjenjat poruku i nes dodatno komplicirat
+                                neighbours_dict[f'robot_{j}'].append(self.odoms[i]) #j-ti robot prima odometriju od i-tog i uskladuje se (mice se) s obzirom na to
 
-                        if self.adjacency_matrix[i, j]: #ako i-ti šalje j-tom svoju odometriju (poziciju) (ako self.adjacency_matrix[i, j] nije 0)
-                            self.odoms[i].child_frame_id = str(i) #da bi mogao vidjet ko je to poslao i onda dobit od njega zetu, a da ne moram mjenjat poruku i nes dodatno komplicirat
-                            neighbours_dict[f'robot_{j}'].append(self.odoms[i]) #j-ti robot prima odometriju od i-tog i uskladuje se (mice se) s obzirom na to
+            else:
+                # Switching topology
+                if None not in self.odoms: #ako je popunjena lista sa ne None vrijednostima tj popunjena je odom podacima
+                    for i in range(self.num_robots):
+                        for j in range(self.num_robots):
+                            if i == j:
+                                continue
+                            #print(self.num_robots)
+                            if self.adjacency_matrix[i, j]:
+                                position_i = self.odoms[i].twist.twist.linear
+                                position_j = self.odoms[j].twist.twist.linear
+                                
+                                #print("poristions")
+                                #print(position_i)
+                                #print(position_j)
+                                euclidean_distance = math.sqrt( (position_i.x - position_j.x)**2 + (position_i.y - position_j.y)**2 )
+                                
+                                print(euclidean_distance)
 
+                                if euclidean_distance < self.radius_value:
+                                    self.odoms[i].child_frame_id = str(i) #da bi mogao vidjet ko je to poslao i onda dobit od njega zetu, a da ne moram mjenjat poruku i nes dodatno komplicirat
+                                    neighbours_dict[f'robot_{j}'].append(self.odoms[i]) #j-ti robot prima odometriju od i-tog i uskladuje se (mice se) s obzirom na to
+                                else:
+                                    pass
+                                    print("uso")
+                                
 
             #publish neighours
             for i, pub in enumerate(self.publisher_neighbours):
                 msg = Neighbours()
                 msg.neighbours_odoms = neighbours_dict[f"robot_{i}"]
                 pub.publish(msg)
-
+            
 
             self.rate.sleep()
             pass
